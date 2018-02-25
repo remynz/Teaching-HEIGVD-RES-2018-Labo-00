4d3b90e2bd01560c5758e6bf57b4d4bdff36b8bd
remy.nasserza <remy.nasserzare@heig-vd.ch>
2018-02-25 07:43:27 +0100

    success mais avec warning

--- Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/Flute.java
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/Flute.java
@@ -1,5 +1,20 @@
 package ch.heigvd.res.lab00;
 
 class Flute extends IInstrument {
-  
+  private int volume = 5;
+  private String color = "black";
+  private String sound = "tut";
+
+  public int getSoundVolume(){
+    return volume;
+  }
+
+  public String play(){
+  	return sound;
+  }
+
+  public String getColor(){
+  	return color;
+  }
+
 }
--- Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/Trumpet.java
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/Trumpet.java
@@ -1,35 +1,21 @@
 package ch.heigvd.res.lab00;
 
-/**
- * This is a very simple class used to demonstrate the specify-implement-validate
- * cycle. All methods used in the JUnit test are defined, so the test class will
- * compile. There is a bug in the add method, so one of the tests is expected
- * to fail.
- * 
- * If you look at the class named ch.heigvd.res.lab01.ApplicationTest, you will
- * find an executable specification for this class. The test methods specify
- * the expected behavior for this class.
- * 
- * @author Olivier Liechti
- */
-public class Application {
+class Trumpet extends IInstrument {
+  private String color = "golden";
+  private int volume = 10;
+  private String sound = "pouet";
 
-  private String message;
-
-  public Application() {
-    this("HEIG-VD rocks!");
+  public String play(){
+    return sound;
   }
 
-  public Application(String message) {
-    this.message = message;
+  public String getColor(){
+    return color;
   }
 
-  public String getMessage() {
-    return message;
-  }
-
-  public int add(int a, int b) {
-    return a + b;
+  public int getSoundVolume(){
+    return volume;
   }
 
 }
+
